d05943f7e704a35f8969988e4d26a971e6f09849
pawel <pszynk@gmail.com>
2016-05-09 13:20:50 +0200

    javascript - js2-mode:
    use eshint with local binary (search)
    disable ejshint

--- spacemacs.d/config/config-javascript.el
+++ spacemacs.d/config/config-javascript.el
@@ -15,8 +15,38 @@
     (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
     (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
     (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil)))
+
+  (defun psz//use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (global-eslint (executable-find "eslint"))
+           (local-eslint (expand-file-name "node_modules/.bin/eslint"
+                                           root))
+           (eslint (if (file-executable-p local-eslint)
+                       local-eslint
+                     global-eslint))
+           (message "print eslint:")
+           (message eslint)
+           )
+      (setq-local flycheck-javascript-eslint-executable eslint)))
+
+  ;; (with-eval-after-load 'flycheck
+  ;;   (flycheck-add-mode 'javascript-eslint 'js2-mode))
+
+  (add-hook 'js2-mode-hook #'psz//use-eslint-from-node-modules)
+
+  (with-eval-after-load 'js2-mode
+    (setq-default flycheck-disabled-checkers
+                  (append flycheck-disabled-checkers '(javascript-jshint))))
   )
+
+
 
 (psz//config-javascript)
 
 (provide 'config-javascript)
+
+
+
+
